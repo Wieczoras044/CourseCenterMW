37bb9a964b7b13fb8da853d868f31104f65d9d70
Wieczoras044 <56131892+Wieczoras044@users.noreply.github.com>
2021-10-24 21:12:06 +0200

    master

--- coursecenter/settings.py
+++ coursecenter/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,5 +130,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-import django_heroku
-django_heroku.settings(locals())
+
