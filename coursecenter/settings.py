1f32e3b1ca7639249540f34a258b9afd2996f94e
Wieczoras044 <56131892+Wieczoras044@users.noreply.github.com>
2021-10-24 21:16:53 +0200

    master

--- coursecenter/settings.py
+++ coursecenter/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'courses/static')
 
 from django.urls import reverse_lazy
 LOGIN_REDIRECT_URL = reverse_lazy('students_course_list')
@@ -129,5 +129,5 @@ LOGIN_REDIRECT_URL = reverse_lazy('students_course_list')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/css'),)
 
