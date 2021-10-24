31805f1d7aa9e8f0c76b8583b2ec5645a0849d06
Wieczoras044 <56131892+Wieczoras044@users.noreply.github.com>
2021-10-24 21:38:46 +0200

    master

--- coursecenter/settings.py
+++ coursecenter/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, '/courses/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '/courses/static/')
 
 from django.urls import reverse_lazy
 LOGIN_REDIRECT_URL = reverse_lazy('students_course_list')
@@ -132,6 +132,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'courses/static/'),
+    os.path.join(BASE_DIR, 'courses/static/'),
 ]
 django_heroku.settings(locals())
