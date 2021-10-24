92869e3a2f35d25954aa11fa78fd7d9861e2f935
Wieczoras044 <56131892+Wieczoras044@users.noreply.github.com>
2021-10-24 21:31:12 +0200

    master

--- coursecenter/settings.py
+++ coursecenter/settings.py
@@ -130,7 +130,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
+    os.path.join(BASE_DIR, 'courses/static/'),
 ]
-
