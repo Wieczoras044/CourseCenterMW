2881a4d4bf138f14c84f88105ff68226c45eec80
Wieczoras044 <56131892+Wieczoras044@users.noreply.github.com>
2021-10-24 21:04:14 +0200

    master

--- coursecenter/settings.py
+++ coursecenter/settings.py
@@ -131,4 +131,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+import django_heroku
 django_heroku.settings(locals())
